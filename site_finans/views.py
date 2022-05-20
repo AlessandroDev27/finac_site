ced595b47c30a91a5c3716eb3ddc8f443ff8f83a
Alessandro Nascimento <94177003+AlessandroDev27@users.noreply.github.com>
2022-05-20 14:25:49 -0300

    Testando conhecimentos

---
+++ site_finans/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def recursos(request):
+    return render(request, 'recursos.html')
+
+
+def beneficios(request):
+    return render(request, 'beneficios.html')
+
+
+def precos(request):
+    return render(request, 'precos.html')
